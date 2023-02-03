0880b8028394df0e50d26935dc5a1cd02f115860
Hardik Gajera <123854368+Hardik8140@users.noreply.github.com>
2023-02-03 09:14:48 +0530

    comments
    what we are print

--- Mathematical Operator/Square and Sum.js
+++ Mathematical Operator/Square and Sum.js
@@ -2,4 +2,4 @@ let one = 1;
 let two = 2;
 let three = 3;
 
-console.log((one*one)+(two*two)+(three*three))
+console.log((one*one)+(two*two)+(three*three)) // print the square and the sum of the given data
